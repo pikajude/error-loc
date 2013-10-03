ae8e1a485f77e0fedda5697924a2b7bc640dbde6
Joel <me@joelt.io>
2013-10-03 16:25:17 -0700

    relocate example

--- ErrorLoc.hs
+++ ErrorLoc.hs
@@ -1,19 +1,30 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE QuasiQuotes #-}
 
-module ErrorLoc (
-  errorLoc
-) where
+-- |
+--
+-- > {-# LANGUAGE TemplateHaskell #-}
+-- >
+-- > import Prelude hiding (error)
+-- > import ErrorLoc
+-- >
+-- > main :: IO ()
+-- > main = $error "Oh no!"
+--
+-- > test.hs:7:10: Oh no!
+module ErrorLoc (error) where
 
 import Language.Haskell.TH
+import Prelude hiding (error)
+import qualified Prelude as P
 
--- | Provides a version of 'error' with call-site metadata.
-errorLoc :: Q Exp
-errorLoc = do
+-- | Provides a version of 'Prelude.error' with call-site metadata.
+error :: Q Exp
+error = do
     loc <- location
-    [e|(.) error (concat [$(stringE (loc_filename loc))
-                       , ":"
-                       , $(stringE . show . fst $ loc_start loc)
-                       , ":"
-                       , $(stringE . show . snd $ loc_start loc)
-                       , ": "] ++)|]
+    [e|(.) P.error (concat [$(stringE (loc_filename loc))
+                           , ":"
+                           , $(stringE . show . fst $ loc_start loc)
+                           , ":"
+                           , $(stringE . show . snd $ loc_start loc)
+                           , ": "] ++)|]
